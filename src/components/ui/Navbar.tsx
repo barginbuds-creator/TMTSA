f6abcedd1ad092cc7fc683ed455a7f5ec94816a1
Kyle <kyle@gssolutions.co.za>
2026-02-02 05:25:57 +0200

    Fix(Navbar): Remove white logo background; implement dynamic White/Black logo on scroll

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -25,7 +25,7 @@ export const Navbar = () => {
                 {/* Logo */}
                 <Link href="/" className="relative h-12 w-32 md:h-16 md:w-48 transition-transform hover:scale-105">
                     <Image
-                        src="/images/tmt-logo-new.png"
+                        src={scrolled ? "/images/tmt-logo-black.png" : "/images/tmt-logo-white.png"}
                         alt="The Maintenance Team"
                         fill
                         className="object-contain"
